5556acc347d35b64a8868d2f54ed1291778922a9
Michal Dolenský <the777misa@gmail.com>
2019-04-27 00:46:52 +0200

    Fixed typo in class name CollisionTestMaze

--- projectmaze/src/main/java/maze/CollisionTestMaze.java
+++ projectmaze/src/main/java/maze/CollisionTestMaze.java
@@ -6,7 +6,7 @@ import blocks.Teleporter;
 import blocks.Wall;
 import transforms.Point3D;
 
-public class ColisionTestMaze extends AbstractMaze {
+public class CollisionTestMaze extends AbstractMaze {
     AbstractBlock[][] level0 = new AbstractBlock[][]{
             {new Wall(), new Wall(), new Wall(), new Wall(), new Wall()},
             {new Wall(), new Hall(), new Teleporter(2, 0, 1), new Hall(), new Wall()},
@@ -14,7 +14,8 @@ public class ColisionTestMaze extends AbstractMaze {
             {new Wall(), new Hall(), new Hall(), new Hall(), new Wall()},
             {new Wall(), new Wall(), new Wall(), new Wall(), new Wall()}
     };
-    public ColisionTestMaze() {
+
+    public CollisionTestMaze() {
         setStartPosition(new Point3D(1, 0, 2));
         getPlayer().setPos(calcPos(getStartPosition()));
         getLevels().add(level0);
