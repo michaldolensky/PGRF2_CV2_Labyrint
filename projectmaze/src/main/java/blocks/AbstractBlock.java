d1495859de3f8f14e21143bab54ad9bd32201c51
Michal Dolenský <dolensky.michal@gmail.com>
2019-04-30 22:09:13 +0200

    Transfer to pc

--- projectmaze/src/main/java/blocks/AbstractBlock.java
+++ projectmaze/src/main/java/blocks/AbstractBlock.java
@@ -3,7 +3,12 @@ package blocks;
 import com.jogamp.opengl.util.texture.Texture;
 
 public class AbstractBlock{
-    Texture texTop;
+    int texN;
+    int texE;
+    int texS;
+    int texW;
+    int texU;
+    int texD;
     boolean collision = false;
     boolean createWall = false;
 
@@ -14,5 +19,29 @@ public class AbstractBlock{
 
     public boolean isCreateWall() {
         return createWall;
+    }
+
+    public int getTexN() {
+        return texN;
+    }
+
+    public int getTexE() {
+        return texE;
+    }
+
+    public int getTexS() {
+        return texS;
+    }
+
+    public int getTexW() {
+        return texW;
+    }
+
+    public int getTexU() {
+        return texU;
+    }
+
+    public int getTexD() {
+        return texD;
     }
 }
--- projectmaze/src/main/java/blocks/Hall.java
+++ projectmaze/src/main/java/blocks/Hall.java
@@ -1,46 +1,24 @@
 package blocks;
 
-import com.jogamp.opengl.GL;
-import com.jogamp.opengl.GL2;
-import com.jogamp.opengl.util.texture.Texture;
-import maze.AbstractMaze;
-import transforms.Vec3D;
-
 public class Hall extends AbstractBlock {
-    GL2 gl;
-    Texture texFloor;
-    Texture texCeiling;
-    int size;
-    Vec3D position;
-    Vec3D color;
-    AbstractMaze maze;
 
     public Hall() {
         createWall = true;
+        this.texN = 1;
+        this.texE = 3;
+        this.texS = 2;
+        this.texW = 4;
+        this.texU = 6;
+        this.texD = 0;
     }
 
-    public Hall init(GL2 gl, AbstractMaze maze, Vec3D position, Vec3D color) {
-        this.gl = gl;
-        this.size = maze.getSquareSize();
-        this.position = position;
-        this.color = color;
-        this.maze = maze;
-        return this;
-    }
-    public void draw() {
-        gl.glEnable(GL.GL_TEXTURE_2D);
-        gl.glActiveTexture(GL.GL_TEXTURE1);
-        gl.glBegin(GL2.GL_QUADS);
-        gl.glColor3f(0.0f, 0.0f, 1.0f);
-        gl.glTexCoord2f(0, 1);
-        gl.glVertex3d(position.getX() * size, position.getY(), position.getZ() * size);
-        gl.glTexCoord2f(1, 1);
-        gl.glVertex3d(size * position.getX() + size, position.getY(), position.getZ() * size);
-        gl.glTexCoord2f(1, 0);
-        gl.glVertex3d(position.getX() * size + size, position.getY(), position.getZ() * size + size);
-        gl.glTexCoord2f(0, 0);
-        gl.glVertex3d(position.getX() * size, position.getY(), size * position.getZ() + size);
-        gl.glEnd();
-        gl.glDisable(GL.GL_TEXTURE_2D);
+    public Hall(int texN, int texE, int texS, int texW, int texU, int texD) {
+        this();
+        this.texN = 1;
+        this.texE = 3;
+        this.texS = 2;
+        this.texW = 4;
+        this.texU = 6;
+        this.texD = 0;
     }
 }
